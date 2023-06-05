32e41641d215e539532c7754ed3c02de3d5eea70
Sasha Goloshchapov <ferrata@hey.com>
2023-06-04 21:19:20 -0500

    fix: package

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
+  // plugins: ["@typescript-eslint"],
   extends: "next/core-web-vitals",
   parserOptions: {
     ecmaVersion: 2018,
